acaf2169f29c2a71ae1246aa05bd725e69eab14e
Andrei Ahureikin <Andrei.Ahureikin@itechart-group.com>
2022-04-27 17:52:21 +0300

    separate app and server

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Express } from 'express';
+import errorHandler from './error/ErrorHandling.middleware';
+import router from './common/routes';
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ parameterLimit: 10 }));
+app.use(errorHandler);
+app.use(router);
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,12 @@
-import express, { Express } from 'express';
 import { PORT } from './config/constants';
-import errorHandler from './error/ErrorHandling.middleware';
-import router from './common/routes';
-import cashService from './cash/cash.service';
 
+import app from './app';
+import cashService from './cash/cash.service';
 import DistrictService from './district/district.service';
 import geocodingService from './geocding/geocoding.service';
 import nominatimService from './locationProviders/nominatim/nominatim.service';
 
 const port = Number(PORT ?? 8000);
-const app: Express = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ parameterLimit: 10 }));
-app.use(errorHandler);
-app.use(router);
 
 geocodingService.addProviders(nominatimService);
 
